cdca694fb961a3c2fa1f59d21c50cd44b7948b92
yb300k <harapuyo@mx4.ttcn.ne.jp>
2016-12-31 18:58:30 +0900

    fix

--- app/app.py
+++ app/app.py
@@ -416,7 +416,7 @@ def handle_text_message(event):
                                 impact_msg = getAttackImpact(getEnemyId(sourceId),num_matcher.group(0))
 
                                 line_bot_api.push_message(getEnemyId(sourceId),
-                                    TextSendMessage(text=str(num_matcher.group(0)).encode('utf-8') + u'への攻撃を受けました。' + str(impact_msg).encode('utf-8'))
+                                    TextSendMessage(text=str(num_matcher.group(0)).encode('utf-8') + u'への攻撃を受けました。' + str(impact_msg).encode('utf-8')))
                                 if impact_msg != '':
                                     line_bot_api.push_message(sourceId,TextSendMessage(text=impact_msg))
                                 else:
