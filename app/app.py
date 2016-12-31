da72a3ff125539e9256268fb5d7724032a5cbe01
yb300k <harapuyo@mx4.ttcn.ne.jp>
2016-12-31 19:12:55 +0900

    fix

--- app/app.py
+++ app/app.py
@@ -414,9 +414,10 @@ def handle_text_message(event):
                                 line_bot_api.push_message(sourceId,TextSendMessage(text='その位置には攻撃できません'))
                             else:
                                 impact_msg = getAttackImpact(getEnemyId(sourceId),num_matcher.group(0))
+                                msgtxt = str(num_matcher.group(0)).encode('utf-8') + str(u'に攻撃を受け・・').encode('utf-8') + \
+                                    str(impact_msg).encode('utf-8')
+                                line_bot_api.push_message(getEnemyId(sourceId),TextSendMessage(text=msgtxt))
 
-                                line_bot_api.push_message(getEnemyId(sourceId),
-                                    TextSendMessage(text=str(num_matcher.group(0)).encode('utf-8') + str(u'に攻撃され・・').encode('utf-8') + str(impact_msg).encode('utf-8')))
                                 if impact_msg != '':
                                     line_bot_api.push_message(sourceId,TextSendMessage(text=impact_msg))
                                 else:
