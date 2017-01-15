58c13a9647c752762e0148e8c47d463ee16bb7d2
shin <stms1008@gmail.com>
2017-01-15 21:23:48 +0900

    fix

--- app/const.py
+++ app/const.py
@@ -2,6 +2,8 @@
 
 import os
 
+IMAGEMAP_ELEMENT_WIDTH = 260
+IMAGEMAP_ELEMENT_HEIGHT = 197
 IMG_PATH = os.path.join(os.path.dirname(__file__), 'static', 'map')
 TMP_ROOT_PATH = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 BG_FILE_PATH = os.path.join(os.path.dirname(__file__), 'static', 'map', 'map-700.png')
