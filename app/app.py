fc16ea25e1999f6334be6a66597686126bab1151
yb300k <harapuyo@mx4.ttcn.ne.jp>
2017-01-26 12:35:23 +0900

    button name

--- app/app.py
+++ app/app.py
@@ -66,13 +66,13 @@ def download_imagemap(size):
 
 @app.route('/images/kqbutton/<size>', methods=['GET'])
 def download_imagekq(size):
-    filename = FIELD_IMAGE_FILENAME.format(size)
+    filename = KQ_IMAGE_FILENAME.format(size)
     return send_from_directory(os.path.join(app.root_path, 'static', 'kqbutton'),
             filename)
 
 @app.route('/images/ambutton/<size>', methods=['GET'])
 def download_imageam(size):
-    filename = FIELD_IMAGE_FILENAME.format(size)
+    filename = AM_IMAGE_FILENAME.format(size)
     return send_from_directory(os.path.join(app.root_path, 'static', 'ambutton'),
             filename)
 
